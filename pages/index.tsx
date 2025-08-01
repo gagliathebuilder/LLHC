a1bcd76d47b072cd981d43282f02ded67a88951f
gagliathebuilder <chrisgaglia@gmail.com>
2025-08-01 13:02:58 -0700

    Update Shark Bait Teaser poster image to use face.png instead of Blue Skull.png

--- pages/index.tsx
+++ pages/index.tsx
@@ -344,7 +344,7 @@ const Home = () => {
                   videoUrl="/videos/Shark Bait Intro.mp4"
                   title="Shark Bait Teaser"
                   description="Coming soon: Ocean adventures await with tear-free washing"
-                  posterImage="/images/Blue Skull.png"
+                  posterImage="/images/face.png"
                   className="w-full"
                 />
                 <VideoSection
